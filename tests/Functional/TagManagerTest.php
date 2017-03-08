4f3bb0ea4e593aaa38dc3ca0cc576b8054448ecc
neustadt <dneustadt@webmatch.de>
2017-03-08 14:59:23 +0100

    added functional tests

--- tests/Functional/TagManagerTest.php
+++ tests/Functional/TagManagerTest.php
@@ -1,7 +1,5 @@
 <?php
 
-use Doctrine\Common\Cache\FilesystemCache;
-
 class TagManagerTests extends Enlight_Components_Test_Controller_TestCase
 {
     /**
@@ -10,20 +8,23 @@ class TagManagerTests extends Enlight_Components_Test_Controller_TestCase
     private $pluginConfig;
 
     /**
-     * @var \Doctrine\DBAL\Connection
+     * @var \WbmTagManager\Services\TagManagerVariables
      */
-    private $connection;
+    private $variables;
 
     public function setUp()
     {
         parent::setUp();
 
         $this->pluginConfig = Shopware()->Container()->get('shopware.plugin.cached_config_reader')->getByPluginName('WbmTagManager');
-        $this->connection = Shopware()->Container()->get('dbal_connection');
+        $this->variables = Shopware()->Container()->get('wbm_tag_manager.variables');
         $this->dispatch('/');
     }
 
-    /**
-     * TODO: add tests
-     */
+    public function testDataLayerVariables()
+    {
+        $dataLayerVariables = $this->variables->getVariables();
+
+        $this->assertTrue($dataLayerVariables["google_tag_params"]["ecomm_pagetype"] === "home");
+    }
 }
