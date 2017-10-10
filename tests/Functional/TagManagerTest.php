634a0955dabf3e6c9e49e5e15ff8f9f0dc8050f6
neustadt <dneustadt@webmatch.de>
2017-10-10 17:21:47 +0200

    dataLayer Service Test

--- tests/Functional/TagManagerTest.php
+++ tests/Functional/TagManagerTest.php
@@ -18,19 +18,14 @@ class TagManagerTests extends Enlight_Components_Test_Controller_TestCase
 
         $this->pluginConfig = Shopware()->Container()->get('shopware.plugin.cached_config_reader')->getByPluginName('WbmTagManager');
         $this->variables = Shopware()->Container()->get('wbm_tag_manager.variables');
-        if (empty($this->pluginConfig['wbmTagManagerContainer'])) {
-            $plugin = Shopware()->Container()->get('shopware_plugininstaller.plugin_manager')->getPluginByName('WbmTagManager');
-            Shopware()->Container()
-                ->get('shopware_plugininstaller.plugin_manager')
-                ->saveConfigElement($plugin, 'wbmTagManagerContainer', 'GTM-XXXXXX');
-        }
         $this->dispatch('/');
     }
 
     public function testDataLayerVariables()
     {
+        $this->variables->setVariables(true);
         $dataLayerVariables = $this->variables->getVariables();
 
-        $this->assertTrue($dataLayerVariables["google_tag_params"]["ecomm_pagetype"] === "home");
+        $this->assertTrue($dataLayerVariables);
     }
 }
