fb7508f0ec3ddb8a50abef1e0f1b54f8de537f62
David Neustadt <saberdude@googlemail.com>
2018-06-27 02:32:48 +0200

    update sql

--- Resources/sql/update.3.0.0.sql
+++ Resources/sql/update.3.0.0.sql
@@ -3,6 +3,14 @@ ALTER TABLE `wbm_data_layer_modules` ADD UNIQUE INDEX `module_UNIQUE` (`module` 
 ALTER TABLE `wbm_data_layer_modules`
 ADD `predispatch` BOOLEAN DEFAULT '0';
 
+UPDATE `wbm_data_layer_modules`
+SET `predispatch` = 1
+WHERE `module` = 'frontend_checkout_ajaxdeletearticlecart';
+
 UPDATE `wbm_data_layer_properties`
 SET `value` = '{if $sCategoryContent.name}{$sCategoryContent.name|escape}{elseif $smarty.request.c}{dbquery select=\'description\' from=\'s_categories\' where=[\'id =\' => $smarty.request.c]}{/if}'
 WHERE `id` = 10 AND `name` = 'category';
+
+UPDATE `wbm_data_layer_properties`
+SET `value` = '{dbquery select=\'ordernumber\' from=\'s_order_basket\' where=[\'id =\' => {request_get param=\'sDelete\'}]}'
+WHERE `id` = 38 AND `name` = 'id';
