f883aecbcffbc87c9f409f2193a1c4bc078a6f4c
neustadt <dneustadt@webmatch.de>
2018-06-28 11:27:22 +0200

    install and update sql

--- Resources/sql/update.3.0.0.sql
+++ Resources/sql/update.3.0.0.sql
@@ -8,14 +8,22 @@ SET `predispatch` = 1
 WHERE `module` = 'frontend_checkout_ajaxdeletearticlecart';
 
 UPDATE `wbm_data_layer_properties`
-SET `value` = '{if $sCategoryContent.name}{$sCategoryContent.name|escape}{elseif $smarty.request.c}{dbquery select=\'description\' from=\'s_categories\' where=[\'id =\' => $smarty.request.c]}{/if}'
-WHERE `id` = 10 AND `name` = 'category';
+SET `value` = '{$sArticle.ordernumber|escape|to_string}'
+WHERE `id` = 7 AND `value` = '{$sArticle.ordernumber|escape}';
 
 UPDATE `wbm_data_layer_properties`
-SET `value` = '{dbquery select=\'ordernumber\' from=\'s_order_basket\' where=[\'id =\' => {request_get param=\'sDelete\'}]}'
-WHERE `id` = 38 AND `name` = 'id';
+SET `value` = '{$article.ordernumber|escape|to_string}'
+WHERE `id` = 17 AND `value` = '{$article.ordernumber|escape}';
+
+UPDATE `wbm_data_layer_properties`
+SET `value` = '{if $sCategoryContent.name}{$sCategoryContent.name|escape}{elseif $smarty.request.c}{{dbquery select=\'description\' from=\'s_categories\' where=[\'id =\' => $smarty.request.c]}|escape}{/if}'
+WHERE `id` = 10 AND `value` = '{$sCategoryContent.name|escape}';
+
+UPDATE `wbm_data_layer_properties`
+SET `value` = '{{dbquery select=\'ordernumber\' from=\'s_order_basket\' where=[\'id =\' => {request_get param=\'sDelete\'}]}|escape|to_string}'
+WHERE `id` = 38 AND `value` = '{$smarty.request.sDelete}';
 
 INSERT IGNORE INTO `wbm_data_layer_properties` (`id`, `module`, `parentID`, `name`, `value`) VALUES
   (108, 'frontend_checkout_ajaxaddarticlecart', 28, 'price', '{dbquery select=\'price\' from=\'s_order_basket\' where=[\'ordernumber =\' => $smarty.request.sAdd, \'sessionID =\' => $smarty.session.Shopware.sessionId] order=[\'id\' => \'DESC\']}'),
   (109, 'frontend_checkout_ajaxdeletearticlecart', 37, 'price', '{dbquery select=\'price\' from=\'s_order_basket\' where=[\'id =\' => {request_get param=\'sDelete\'}]}'),
-  (110, 'frontend_checkout_ajaxdeletearticlecart', 37, 'quantity', '1');
+  (110, 'frontend_checkout_ajaxdeletearticlecart', 37, 'quantity', '{dbquery select=\'quantity\' from=\'s_order_basket\' where=[\'id =\' => {request_get param=\'sDelete\'}]}');
